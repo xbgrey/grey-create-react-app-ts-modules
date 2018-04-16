b56f2b41c253f6c211d74dbd87d66dca34e129c0
xb_grey <xb_grey@163.com>
2018-04-16 21:30:53 +0800

    基础包提炼

--- src/node_modules/kts-scaffold-framework/message/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Event } from './Event'; // 事件件对象
-export { default as Message } from './Message'; // 消息对象 7b38f887-e889-496c-b92d-d1739afa436e
--- src/utils/kit/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as JSObject } from './JSObject'; // 对象操作
-export { default as JSString } from './JSString'; // 字符串操作
