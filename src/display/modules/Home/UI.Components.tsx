ae65f89a9c06b8171c3311247794584e806f6bcf
xb_grey <xb_grey@163.com>
2018-04-19 22:58:23 +0800

    解决了antd Form 修饰器bug

--- src/display/modules/Home/UI.Components.tsx
+++ src/display/modules/Home/UI.Components.tsx
@@ -4,6 +4,8 @@ import { connect } from 'src/redux';
 import ReduxState, { } from 'src/redux/ReduxState';
 import ModulesState from './Modules.State';
 import ModulesAction from './Modules.Action';
+import { FormComponentProps } from 'antd/lib/form';
+import { Form, Input } from 'antd';
 
 /** 全局数据片段数据接口 */
 interface IReduxStatePart {
@@ -11,14 +13,14 @@ interface IReduxStatePart {
 }
 
 /** 组建的props接口 */
-interface IProps extends IReduxStatePart, IPropsBasic {
+interface IProps extends IReduxStatePart, IPropsBasic, FormComponentProps {
 
 }
 
 /** 绑定全局数据到props */
 @connect((state: ReduxState): IReduxStatePart => ({
 
-}))
+}), true)
 export default class UIComponents extends UIBasic<IProps, ModulesState> {
 
     /** 构造函数 */
@@ -27,11 +29,27 @@ export default class UIComponents extends UIBasic<IProps, ModulesState> {
     }
     
     render() {
-        const { } = this.props; // 获取你的props数据
+        const { form } = this.props; // 获取你的props数据
+        const { getFieldDecorator } = form;
         return (
             <div>
-                你的组件
+                <Form.Item>
+                    {getFieldDecorator('loginName', {
+                        rules: [
+                            { required: true, },
+                        ],
+                    })(<Input size="large" placeholder="请输入用户名" />)}
+                </Form.Item>
+
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [
+                            { required: true, },
+                        ],
+                    })(<Input size="large" type="password" placeholder="请输入密码" />)}
+                </Form.Item>
             </div>
         );
     }
 }
+ 
--- src/redux/connect.ts
+++ src/redux/connect.ts
@@ -1,14 +1,26 @@
 import { connect } from 'react-redux';
 import { ReduxState } from 'src/redux';
+import { Form } from 'antd';
 
-function connectIndex(reduxStatePart: (state: ReduxState) => any) {
+/**
+ * 绑定组件
+ * @param reduxStatePart 一个返回全局对象片段的方法
+ * @param isForm 是否关联 Form
+ */
+function connectIndex(reduxStatePart: (state: ReduxState) => any, isForm: boolean = false) {
     const s2p = (state: any) => {
         return {
             ...
             reduxStatePart(state)
         };
     };
-    return (dom): any => { return connect(s2p)(dom); };
+    return (dom): any => {
+        if (isForm) {
+            return connect(s2p)(Form.create()(dom));
+        } else {
+            return connect(s2p)(dom);
+        }
+    };
 }
 
 export default connectIndex;
